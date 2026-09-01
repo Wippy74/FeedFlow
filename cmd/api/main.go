c9fb789a8a3d1141fea2265e8258dd564b538ae6
Wippy74 <vsevoloduzaninov@gmail.com>
2026-09-01 23:30:03 +0300

    feat: swapped pgx.Conn for a pgxpool for multithread usage

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,8 +13,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/jackc/pgx/v5"
-	_ "github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
@@ -27,15 +26,23 @@ func main() {
 	log.Println("Connecting to database")
 	migrations.RunMigrations(cfg.DBUrl)
 
-	conn, err := pgx.Connect(context.Background(), cfg.DBUrl)
+	connectCtx, cancelConnect := context.WithTimeout(context.Background(), 10*time.Second)
+	dbPool, err := pgxpool.New(connectCtx, cfg.DBUrl)
 	if err != nil {
-		log.Fatal(err)
-		return
+		cancelConnect()
+		log.Fatalf("failed to create database pool: %v", err)
 	}
-	defer conn.Close(context.Background())
+
+	if err := dbPool.Ping(connectCtx); err != nil {
+		cancelConnect()
+		dbPool.Close()
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	cancelConnect()
+	defer dbPool.Close()
 	log.Println("Connected to database")
 
-	repo := storage.NewRepository(conn)
+	repo := storage.NewRepository(dbPool)
 	cache := cache2.NewRedisCache(cfg.RedisAddr)
 	go func() {
 		err := worker.Start(repo, 1*time.Minute, 3)
--- internal/database/storage/db.go
+++ internal/database/storage/db.go
@@ -1,14 +1,14 @@
 package storage
 
 import (
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Repository struct {
-	db *pgx.Conn
+	db *pgxpool.Pool
 }
 
-func NewRepository(db *pgx.Conn) *Repository {
+func NewRepository(db *pgxpool.Pool) *Repository {
 	return &Repository{
 		db: db,
 	}

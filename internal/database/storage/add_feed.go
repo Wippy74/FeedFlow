28db8152245dd2c4c40869d5f6ece438abcf1807
Wippy74 <vsevoloduzaninov@gmail.com>
2026-09-02 21:14:50 +0300

    fix: insert query

--- internal/database/storage/add_feed.go
+++ internal/database/storage/add_feed.go
@@ -1,19 +1,21 @@
 package storage
 
 import (
-	"NewsAggregator/internal/model"
 	"context"
 	"time"
+
+	"NewsAggregator/internal/model"
 
 	"github.com/google/uuid"
 )
 
 func (repo *Repository) AddFeed(ctx context.Context, id uuid.UUID, name, url string) (model.Feed, error) {
-	query := `INSERT INTO feeds (id, created_at, updated_at, name, url, last_fetched_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, created_at, updated_at, name, url, last_fetched_at`
+	query := `INSERT INTO feeds (id, created_at, updated_at, name, url, last_fetched_at) VALUES ($1, $2, $3, $4, $5, NULL) RETURNING id, created_at, updated_at, name, url, last_fetched_at`
 
 	var newFeed model.Feed
 
-	err := repo.db.QueryRow(ctx, query, id, time.Now().UTC(), time.Now().UTC(), name, url, time.Now().UTC()).Scan(
+	now := time.Now().UTC()
+	err := repo.db.QueryRow(ctx, query, id, now, now, name, url).Scan(
 		&newFeed.ID, &newFeed.CreatedAt, &newFeed.UpdatedAt, &newFeed.Name, &newFeed.Url, &newFeed.LastFetchedAt)
 	if err != nil {
 		return model.Feed{}, err

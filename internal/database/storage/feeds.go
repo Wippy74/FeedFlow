fd3d4c0e0203c01bd8d5e34d8857220e645d919f
Wippy74 <vsevoloduzaninov@gmail.com>
2026-09-02 21:13:28 +0300

    fix: errors handling

--- internal/database/storage/feeds.go
+++ internal/database/storage/feeds.go
@@ -1,15 +1,16 @@
 package storage
 
 import (
-	"NewsAggregator/internal/model"
 	"context"
 	"time"
+
+	"NewsAggregator/internal/model"
 
 	"github.com/google/uuid"
 )
 
 func (repo *Repository) GetNextFeedsToFetch(ctx context.Context, limit int) ([]model.Feed, error) {
-	query := `SELECT id, name, url, last_fetched_at FROM feeds ORDER BY last_fetched_at ASC NULLS FIRST LIMIT $1`
+	query := `SELECT id, name, url, last_fetched_at FROM feeds ORDER BY last_fetched_at ASC NULLS FIRST, id ASC LIMIT $1`
 
 	var feeds []model.Feed
 	raws, err := repo.db.Query(ctx, query, limit)
@@ -26,12 +27,15 @@ func (repo *Repository) GetNextFeedsToFetch(ctx context.Context, limit int) ([]m
 		}
 		feeds = append(feeds, feed)
 	}
+	if err := raws.Err(); err != nil {
+		return nil, err
+	}
 	return feeds, nil
 }
 
 func (repo *Repository) MarkFeedFetched(ctx context.Context, id uuid.UUID) error {
 	query := `UPDATE feeds SET last_fetched_at = $1 WHERE id = $2`
 
-	_, err := repo.db.Exec(ctx, query, time.Now(), id)
+	_, err := repo.db.Exec(ctx, query, time.Now().UTC(), id)
 	return err
 }
--- internal/database/storage/save_post.go
+++ internal/database/storage/save_post.go
@@ -1,15 +1,27 @@
 package storage
 
 import (
-	"NewsAggregator/internal/model"
 	"context"
+	"errors"
 	"time"
+
+	"NewsAggregator/internal/model"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
-func (repo *Repository) SavePost(ctx context.Context, post model.Post) error {
-	query := `INSERT INTO posts (id, created_at, updated_at, title, description, published_at, url, feed_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) ON CONFLICT (url) DO NOTHING;`
+func (repo *Repository) SavePost(ctx context.Context, post model.Post) (bool, error) {
+	query := `INSERT INTO posts (id, created_at, updated_at, title, description, published_at, url, feed_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) ON CONFLICT (url) DO NOTHING RETURNING id;`
 
 	now := time.Now().UTC()
-	_, err := repo.db.Exec(ctx, query, post.ID, now, now, post.Title, post.Description, post.PublishedAt, post.Url, post.FeedID)
-	return err
+	var insertedID uuid.UUID
+	err := repo.db.QueryRow(ctx, query, post.ID, now, now, post.Title, post.Description, post.PublishedAt, post.Url, post.FeedID).Scan(&insertedID)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
